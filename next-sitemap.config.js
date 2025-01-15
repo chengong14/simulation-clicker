2549a8f40ed8e0bc7b4cf9e41e82e716fb9d40ed
samgchen <samgchen@tencent.com>
2025-01-15 20:04:49 +0800

    add code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://simulation-clicker.vercel.app', // 请替换为你的实际网站URL
+  siteUrl: 'https://www.stimulationclick.com', // 请替换为你的实际网站URL
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   sitemapSize: 7000,
   exclude: ['/404'],
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://simulation-clicker.vercel.app/sitemap.xml',
+      'https://www.stimulationclick.com/sitemap.xml',
     ],
   },
 } 
